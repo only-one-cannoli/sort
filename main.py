241cd3d0e711291fb2051e9e641236e6f999c4ef
Patrick Applegate <papplega@gmail.com>
2022-10-29 21:51:59 -0400

    merge sort, better tests

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from copy import deepcopy
 from typing import List
 
 
-def _sorted_insert(target: List[float], to_insert: float) -> None:
+def _sorted_append(target: List[float], to_insert: float) -> List[float]:
     """
     Inserts to_insert into target such that the result remains sorted.  Assumes
     that target is sorted!
@@ -17,13 +18,60 @@ def _sorted_insert(target: List[float], to_insert: float) -> None:
         where = len(target)
     target.insert(where, to_insert)
 
+    return target
+
 
 def insertion_sort(target: List[float]) -> List[float]:
     """
     Insertion sort.
     """
-    ordered = [target[0]]
+    try:
+        ordered = [target[0]]
+    except IndexError:
+        return target
+
     for element in target[1:]:
-        _sorted_insert(ordered, element)
+        _sorted_append(ordered, element)
 
     return ordered
+
+
+def _sorted_extend(list1: List[float], list2: List[float]) -> List[float]:
+    """
+    Extends list1 with the contents of list2.  Assumes that at least list1 is
+    already sorted!
+    """
+    for element in list2:
+        list1 = _sorted_append(list1, element)
+    list2 = []
+
+    return list1
+
+
+def merge_sort(target: List[float]) -> List[float]:
+    """
+    Merge sort.
+    """
+    if len(target) < 1:
+        return []
+
+    split = [[element] for element in target]
+
+    while len(split) > 1:
+
+        if len(split) % 2 == 1:
+            split.append([])
+
+        split = [
+            _sorted_extend(split[i], split[i + 1])
+            for i in range(0, len(split) - 1, 2)
+        ]
+
+    return split[0]
+
+
+def quicksort(target: List[float]) -> List[float]:
+    """
+    Quicksort.
+    """
+
--- test.py
+++ test.py
@@ -1,28 +1,58 @@
-from main import (
-    _sorted_insert,
-    insertion_sort,
-)
 import big_o
+import pytest
+
+from main import (
+    _sorted_append,
+    _sorted_extend,
+    insertion_sort,
+    merge_sort,
+)
 
 
-def test_sorted_insert():
-    tgt = [1, 3]
-    _sorted_insert(tgt, 2)
-    assert tgt == [1, 2, 3]
+@pytest.fixture
+def test_cases():
+    return {
+        "normal": {
+            "pre": [7, 12, 10, 7, 1, 6, 16, 18, 3, 5],
+            "post": [1, 3, 5, 6, 7, 7, 10, 12, 16, 18],
+        },
+        "empty": {"pre": [], "post": []},
+        "one_element": {"pre": [1], "post": [1]},
+        "already_sorted": {
+            "pre": [1, 3, 5, 6, 7, 7, 10, 12, 16, 18],
+            "post": [1, 3, 5, 6, 7, 7, 10, 12, 16, 18],
+        },
+    }
 
 
-to_sort = [3, 1, 5, 2, 7, 7, 8]
-ordered = sorted(to_sort)
+class TestInternalMethods:
+    def test_sorted_append(self):
+        assert _sorted_append([1, 3], 2) == [1, 2, 3]
+
+    def test_sorted_extend(self):
+        assert _sorted_extend([1, 3], [2, 4]) == [1, 2, 3, 4]
 
 
-def test_insertion_sort():
-    assert insertion_sort(to_sort) == ordered
+class TestSortingAlgorithms:
+    def test_insertion_sort(self, test_cases):
+        for case in test_cases.keys():
+            assert (
+                insertion_sort(test_cases[case]["pre"])
+                == test_cases[case]["post"]
+            )
 
 
+    def test_merge_sort(self, test_cases):
+        for case in test_cases.keys():
+            assert (
+                merge_sort(test_cases[case]["pre"])
+                == test_cases[case]["post"]
+            )
+"""
 positive_int_generator = lambda n: big_o.datagen.integers(n, 0, 10000)
 
 
-def test_insertion_sort_quadratic():
+def test_appendion_sort_quadratic():
     best, _ = big_o.big_o(
         insertion_sort,
         positive_int_generator,
@@ -31,3 +61,4 @@ def test_insertion_sort_quadratic():
         max_n=1000,
     )
     assert "Quadratic" in str(best)
+"""
